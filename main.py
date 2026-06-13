68041d782c1626b4ff0fdca96bb5fd3a4a9b57b0
Medo <4medo42009@gmail.com>
2026-06-13 23:47:19 +0300

    upload project

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import os_battery as ob
 import compute
 import time 
 import psutil as ps
+import compute as cp
 import voice_input_and_transcript as vit
 model = "qwen3.5:cloud"
 whisper_model = "tiny"
-compute_device = "cuda"
-compute_type = "float16"
+compute_device = cp.gpu_info().results[0]
+compute_type = "int8"
+oss = ob.get_os(ob.oss)
+match oss:
+    case "windows":
+        dtype = ob.
 
 
 
